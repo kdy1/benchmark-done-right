7afee2fe39a95fe06e92d70d40fc4dc024db2205
강동윤 <kdy1997.dev@gmail.com>
2021-05-13 16:11:20 +0900

    Store benchmark results as json

--- util.ts
+++ util.ts
@@ -1,5 +1,28 @@
+import Benchmark from 'benchmark'
 import { JscTarget } from '@swc/core';
 import * as ts from 'typescript';
+import * as path from 'path';
+import { promises as fs } from 'fs';
+
+export async function store(op: string, res: { [name: string]: number }) {
+    const dataDir = path.join(__dirname, '.data');
+    await fs.mkdir(dataDir, { recursive: true });
+
+    const f = path.join(dataDir, `${op}.json`);
+    await fs.writeFile(f, JSON.stringify(res), 'utf8')
+}
+
+export async function storeResult(op: string, res: Benchmark.Target & Benchmark.Suite) {
+    const reduced = res.reduce((arg: any, v: any) => {
+        return {
+            [v.name]: v.hz,
+            ...arg
+        };
+    }, {});
+
+
+    await store(op, reduced)
+}
 
 export function toTsTarget(target: JscTarget): ts.ScriptTarget {
     switch (target) {
